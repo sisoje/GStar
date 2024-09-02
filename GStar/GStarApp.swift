33b85881d296bcab3b34d88a9d25873bbd42adbc
Lazar Otasevic <>
2024-09-03 01:31:31 +0400

    integrated tests

--- GStar/GStarApp.swift
+++ GStar/GStarApp.swift
@@ -6,8 +6,19 @@
 //
 
 import SwiftUI
+import ViewHosting
 
 @main
+enum AppLauncher {
+    static func main() {
+        if NSClassFromString("XCTestCase") != nil {
+            ViewHostingApp.main()
+        } else {
+            GStarApp.main()
+        }
+    }
+}
+
 struct GStarApp: App {
     var body: some Scene {
         WindowGroup {
