29ad9ebb4eb9f41c13ecc97a996c246d9c97c600
Lazar Otasevic <>
2024-09-03 00:04:13 +0400

    initial

---
+++ GStar/GStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GStarApp.swift
+//  GStar
+//
+//  Created by Lazar Otasevic on 2.9.24..
+//
+
+import SwiftUI
+
+@main
+struct GStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
